5cdee65d3b59a14d8472f429b060e9a8a766f8f7
anonymousb61 <mansvibhatia@gmail.com>
2022-11-23 15:52:03 +0530

    add request, ride details, request details

---
+++ app/src/main/java/com/example/ryder/RequestDetailsViewModel.java
@@ -0,0 +1,9 @@
+package com.example.ryder;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RequestDetailsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+LiveData<Request> requestDetails;
+}
---
+++ app/src/main/java/com/example/ryder/Ride.java
@@ -0,0 +1,15 @@
+package com.example.ryder;
+
+public class Ride {
+    String driverName;
+    String vehicleNumber;
+    String name;
+    String pickup;
+    String destination;
+    String dateTime;
+    String cost;
+    String space;
+    String confirmedPassengers;
+
+
+}
